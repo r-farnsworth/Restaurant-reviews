750cad3e8e0e9beb1527608cfd9f440d7ab43141
Rachael Farnsworth <rachael@farnz.org.uk>
2018-06-26 12:17:42 +0100

    correct bug in service worker

--- js/main.js
+++ js/main.js
@@ -193,7 +193,7 @@ addMarkersToMap = (restaurants = self.restaurants) => {
 Register service worker
 */
 
-if ('!serviceWorker' in navigator) {
+if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('../sw.js')
   .then((reg) => {
     // if registration worked
